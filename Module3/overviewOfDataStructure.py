ac16bc8312075a75671d6a198bfedfc814ed4022
Kushagra Awasthi <Kushagraawasthi37@gmail.com>
2025-12-13 11:20:21 +0530

    Python basics

---
+++ Module3/overviewOfDataStructure.py
@@ -0,0 +1,16 @@
+ # Data Structure -> way of organizing and storing the element ios it can be access and manipulated easily
+
+#List -> Ordered Collection of elements that can be of any datatype, mutable
+
+# Creation of element
+nums=[1,2,3,4,"5"]
+
+# Access element in List
+print(nums[0])
+
+# Adding element at end of list
+nums.append(11)
+
+# Removing the element from end of list
+nums.remove()
+print(nums)
