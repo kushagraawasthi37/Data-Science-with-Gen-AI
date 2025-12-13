66a41bc1fb4107f1fac2ad5cb1cecad05891364f
Kushagra Awasthi <Kushagraawasthi37@gmail.com>
2025-12-13 11:41:52 +0530

    Basic overview of Data structure

--- Module3/overviewOfDataStructure.py
+++ Module3/overviewOfDataStructure.py
@@ -6,11 +6,63 @@
 nums=[1,2,3,4,"5"]
 
 # Access element in List
-print(nums[0])
+# print(nums[0])
 
 # Adding element at end of list
-nums.append(11)
+# nums.append(11)
 
-# Removing the element from end of list
-nums.remove()
-print(nums)
+# Removing the element first occurance from end of list-> raise error if value is not present
+# nums.remove(2)
+
+
+# List inside list
+# nums.append([6,7,8])
+# print(nums[5][2])
+
+
+# Tuples
+point= (2,3)
+# print(type(point))
+
+# print(point[0])
+
+
+s={"red","blue","red",1,2,1}
+# print(s)
+
+# s.add(5)
+# s.remove(2)
+# print(s)
+
+# Dictionary --> (Key,value) pair
+
+my_dict={}
+# print(type(my_dict))
+
+# phoneBook={"Dad":12342,"Mom":1123243}
+# phoneBook["Dad"]=1213# Mutable
+# print(phoneBook.keys())
+# print(phoneBook.values())
+
+
+#Arrays in Python 
+#array is homogeneous and memory-efficient:
+from array import array
+
+a= array('i',[1,2,3,4])
+"""
+
+It tells Python what kind of data the array will store and how it’s stored in memory.
+
+'i' means:
+Signed integer
+
+'i'	Signed integer
+'I'	Unsigned integer
+'f'	Float
+'d'	Double (float64)
+'b'	Signed char
+'B'	Unsigned char
+"""
+print(a[0])
+
